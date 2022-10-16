6c696f47f2a48efa413f808c4e1724dc782770e2
tokileecy <tokileecy@gmail.com>
2022-10-16 19:28:22 +0800

    feat: wait for css loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'normalize.css/normalize.css'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    setTimeout(() => {
+      document.body.classList.remove('hidden')
+    }, 100)
+  }, [])
+
   return <Component {...pageProps} />
 }
 
