920b2cf1e366d169d0436e19f8c070d85f1780c7
tokileecy <tokileecy@gmail.com>
2022-10-16 18:25:54 +0800

    feat: adjust RepoList

--- components/RepoList/RepoList.styles.ts
+++ components/RepoList/RepoList.styles.ts
@@ -3,5 +3,8 @@ import { css } from '@emotion/css'
 export const root = css({
   display: 'flex',
   flexDirection: 'column',
-  gap: '1.5rem',
+  gap: '1.25rem',
+  padding: 0,
+  margin: 0,
+  width: '100%',
 })
--- components/RepoList/RepoList.tsx
+++ components/RepoList/RepoList.tsx
@@ -1,12 +1,19 @@
-import { ReactNode } from 'react'
+import { forwardRef, ReactNode } from 'react'
 import * as styles from './RepoList.styles'
 
 export interface RepoListProps {
   children?: ReactNode
 }
 
-export default function RepoList(props: RepoListProps) {
+export default forwardRef<HTMLUListElement, RepoListProps>(function RepoList(
+  props,
+  ref
+) {
   const { children } = props
 
-  return <ul className={styles.root}>{children}</ul>
-}
+  return (
+    <ul ref={ref} className={styles.root}>
+      {children}
+    </ul>
+  )
+})
