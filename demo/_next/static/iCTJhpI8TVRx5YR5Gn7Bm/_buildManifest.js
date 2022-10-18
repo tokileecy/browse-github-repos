7ba279d766ca9afbb51fcc68b751f2b3ac317170
tokileecy <tokileecy@gmail.com>
2022-10-18 06:28:17 +0000

    Deploying to gh-pages from @ tokileecy/decard-interview@89f5130a4b4b450957bffd5cd0a1419fb9a9e4cd 🚀

---
+++ demo/_next/static/iCTJhpI8TVRx5YR5Gn7Bm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/475-9d40d1ee0947a703.js","static/chunks/pages/index-b47ea21f65c97935.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
